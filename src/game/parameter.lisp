e9959dde75bd4b07852ab6b105174205cf1b1559
eshamster <hamgoostar@gmail.com>
2018-03-25 04:35:08 +0000

    Refactor: Specify parameters about length as relative value to field

--- src/game/parameter.lisp
+++ src/game/parameter.lisp
@@ -5,16 +5,37 @@
   (:export :get-param))
 (in-package :clw-block-braking/src/game/parameter)
 
+(eval-when (:execute :compile-toplevel :load-toplevel)
+  ;; width : height = 3 : 4
+  (defvar.ps+ field-height 560)
+  (defvar.ps+ field-width (/ (* field-height 3.0) 4))
+
+  (defun.ps+ calc-absolute-length (relative-length base-length)
+    (* relative-length base-length 0.001))
+
+  "#Ex1. '#lx500' represents a half length of the field width."
+  "#Ex2. '#ly500' represents a half length of the field height."
+  (set-dispatch-macro-character
+   #\# #\l
+   #'(lambda (stream &rest rest)
+       (declare (ignore rest))
+       (case (peek-char nil stream)
+         (#\x (read-char stream)
+              `(calc-absolute-length ,(read stream) field-width))
+         (#\y (read-char stream)
+              `(calc-absolute-length ,(read stream) field-height))
+         (t (error "Not recognized character after #l"))))))
+
 (defvar.ps+ *params*
   (convert-to-layered-hash
-   (:field (:x 190 :y 20 :width 420 :height 560 :depth 0)
-    :ball (:r 5 :color #xffaaaa
-           :speed (:base (:min 3 :max 6))
+   (:field (:x 190 :y 20 :width field-width :height field-height :depth 0)
+    :ball (:r #lx10 :color #xffaaaa
+           :speed (:base (:min #ly5 :max #ly10))
            :angle (:min (/ PI 7) :max-accele (/ PI 8))
-           :dist-from-paddle 5)
-    :paddle (:width (:min 60 :max 25)
-             :height 6 :depth 8
-             :base-line-height 56  :lane-space 12 :lane-count 4))))
+           :dist-from-paddle #ly10)
+    :paddle (:width (:min #lx130 :max #lx55)
+             :height #ly10 :depth 8
+             :base-line-height #ly80 :lane-space #ly30 :lane-count 4))))
 
 (defmacro.ps+ get-param (&rest keys)
   `(get-layered-hash *params* ,@keys))
