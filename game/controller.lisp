9a58c22b8bee5c0eaa6d25a5b891d13bb380afa0
eshamster <hamgoostar@gmail.com>
2018-05-02 14:25:46 +0000

    Refactor: Integrate mouse controll to one entity

--- game/controller.lisp
+++ game/controller.lisp
@@ -68,6 +68,31 @@
 
 ;; --- initialization --- ;;
 
+(defun.ps+ process-by-mouse (entity)
+  (check-entity-tags entity :mouse)
+  ;; update point
+  (with-ecs-components (point-2d) entity
+    (setf (point-2d-x point-2d) (get-mouse-x)
+          (point-2d-y point-2d) (get-mouse-y)))
+  ;; add gravity to current target
+  (when (eq (get-left-mouse-state) :down-now)
+    (let ((target (get-entity-param entity :current-target)))
+      (if target
+          (add-gravity-to-entity target)
+          (delete-gravity-from-current))))
+  (set-entity-param entity :current-target nil)
+  ;; shoot ball
+  (let ((ball (find-a-entity-by-tag :ball)))
+    (assert ball)
+    (when (= (get-left-mouse-state) :down-now)
+      (shoot-ball ball)))
+  ;; move paddle
+  (let ((paddle (find-a-entity-by-tag :paddle)))
+    (let ((wheel (get-mouse-wheel-delta-y)))
+      (unless (= wheel 0)
+        (change-paddle-lane paddle (< wheel 0))))
+    (move-paddle-to paddle (get-mouse-x))))
+
 (defun.ps+ make-mouse-entity ()
   (let ((mouse (make-ecs-entity))
         (r 3))
@@ -91,38 +116,9 @@
                     (< (calc-dist-p2 mine (calc-global-point target))
                        (calc-dist-p2 mine (calc-global-point pre-target))))
             (set-entity-param mine :current-target target)))))
-     (make-script-2d
-      :func (lambda (entity)
-              ;; update point
-              (with-ecs-components (point-2d) entity
-                (setf (point-2d-x point-2d) (get-mouse-x)
-                      (point-2d-y point-2d) (get-mouse-y)))
-              ;; add gravity to current target
-              (when (eq (get-left-mouse-state) :down-now)
-                (let ((target (get-entity-param entity :current-target)))
-                  (if target
-                      (add-gravity-to-entity target)
-                      (delete-gravity-from-current))))
-              (set-entity-param entity :current-target nil)))
+     (make-script-2d :func #'process-by-mouse)
      (init-entity-params :current-target nil))
     mouse))
 
 (defun.ps+ init-controller ()
-  (add-ecs-entity (make-mouse-entity))
-  (let ((controller (make-ecs-entity)))
-    (add-entity-tag controller :controller)
-    (add-ecs-component-list
-     controller
-     (make-script-2d
-      :func (lambda (_)
-              (declare (ignore _))
-              (let ((ball (find-a-entity-by-tag :ball)))
-                (assert ball)
-                (when (= (get-left-mouse-state) :down-now)
-                  (shoot-ball ball)))
-              (let ((paddle (find-a-entity-by-tag :paddle)))
-                (let ((wheel (get-mouse-wheel-delta-y)))
-                  (unless (= wheel 0)
-                    (change-paddle-lane paddle (< wheel 0))))
-                (move-paddle-to paddle (get-mouse-x))))))
-    (add-ecs-entity controller)))
+  (add-ecs-entity (make-mouse-entity)))
