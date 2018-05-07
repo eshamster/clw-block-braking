f068e0e08b3a83290eaea8f557e30b8653c60357
eshamster <hamgoostar@gmail.com>
2018-05-07 13:05:12 +0000

    Change: Cleck is triggerred when detecting up and down on the same entity

--- game/ui.lisp
+++ game/ui.lisp
@@ -12,6 +12,8 @@
 (defstruct.ps+ (ui-component (:include ecs-component))
     on-click)
 
+(defvar.ps+ *current-target* nil)
+
 (defstruct.ps+
     (ui-system
      (:include
@@ -21,16 +23,23 @@
        (lambda (entity)
          (with-ecs-components (point-2d physic-2d ui-component) entity
            (when (ui-component-on-click ui-component)
-             (let ((mouse-pnt (make-point-2d :x (get-mouse-x)
-                                             :y (get-mouse-y)))
-                   (mouse-physic (make-physic-circle :r 0)))
-               (when (collide-physics-p physic-2d point-2d
-                                        mouse-physic mouse-pnt)
-                 (add-to-monitoring-log (+ "Hover on UI: " (ecs-entity-id entity)))
-                 (when (eq (get-left-mouse-state) :up-now)
-                   (let ((on-click (ui-component-on-click ui-component)))
-                     (when on-click
-                       (funcall on-click nil)))))))))))))
+             (let* ((mouse-pnt (make-point-2d :x (get-mouse-x)
+                                              :y (get-mouse-y)))
+                    (mouse-physic (make-physic-circle :r 0))
+                    (collide-p (collide-physics-p physic-2d point-2d
+                                                  mouse-physic mouse-pnt)))
+               (case (get-left-mouse-state)
+                 (:down-now
+                  (when collide-p
+                    (setf *current-target* entity)))
+                 (:up-now
+                  (when (and collide-p
+                             (eq *current-target* entity)
+                             (find-the-entity entity))
+                    (let ((on-click (ui-component-on-click ui-component)))
+                      (when on-click
+                        (funcall on-click nil))))
+                  (setf *current-target* nil)))))))))))
 
 (defun.ps+ init-ui-system ()
   (make-ui-system))
