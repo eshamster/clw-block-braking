a50f4479504b3a926ba2add3d9d24a15485ce929
eshamster <hamgoostar@gmail.com>
2018-05-20 06:36:41 +0000

    Refactor: Separate stage-selector from menu-state file

--- game/state/menu.lisp
+++ game/state/menu.lisp
@@ -4,53 +4,15 @@
         :cl-ps-ecs
         :cl-web-2d-game)
   (:export :make-game-menu-state)
+  (:import-from :clw-block-braking/game/state/menu_stage-selector
+                :make-stage-selector
+                :generate-stage-list)
   (:import-from :clw-block-braking/game/ui
                 :make-ui-component)
-  (:import-from :clw-block-braking/game/stage-generator
-                :get-max-stage-number)
   (:import-from :clw-block-braking/game/state/utils
                 :make-state
                 :def-game-state))
 (in-package :clw-block-braking/game/state/menu)
-
-;; --- stage selector --- ;;
-
-(defun.ps+ increment-selector (selector delta)
-  (aset-entity-param selector :selected
-                     (mod (+ it delta)
-                          (length (get-entity-param selector :choices))))
-  ;; TODO: (temporal)
-  (add-to-event-log
-   (nth (get-entity-param selector :selected) (get-entity-param selector :choices))))
-
-(defun.ps+ generate-all-stage-list ()
-  (loop for i from 1 to (get-max-stage-number)
-     collect i))
-
-(defun.ps+ generate-stage-list (selector)
-  (let ((target (nth (get-entity-param selector :selected)
-                     (get-entity-param selector :choices))))
-    (case target
-      (:all (generate-all-stage-list))
-      (t (list target)))))
-
-(defun.ps+ make-stage-selector ()
-  (let ((selector (make-ecs-entity))
-        (choices (generate-all-stage-list)))
-    (push :all choices)
-    (add-entity-tag selector :stage-selector)
-    (add-ecs-component-list
-     selector
-     (make-script-2d
-      :func (lambda (entity)
-              (let ((wheel-delta (get-mouse-wheel-delta-y)))
-                (cond ((> wheel-delta 0)
-                       (increment-selector entity 1))
-                      ((< wheel-delta 0)
-                       (increment-selector entity -1))))))
-     (init-entity-params :choices choices
-                         :selected 0))
-    selector))
 
 ;; --- mouse --- ;;
 
